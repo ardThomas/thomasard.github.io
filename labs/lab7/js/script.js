dd04ed94edd500aaf2110ace69e461db6def9776
ardThomas <ardthomas0628@gmail.com>
2025-11-14 14:48:16 -0600

    Update script.js

--- labs/lab7/js/script.js
+++ labs/lab7/js/script.js
@@ -1,15 +1,8 @@
 function init(){
 //add your javascrip between these two lines of code
  
-  let button = document.getElementById("entrybutton");
 
-  function myFunction(){
-    let userText = document.getElementById("entryinput").value;
-    alert(userText);
-    document.getElementById("textoutput").innerHTML = userText;
-  }
-
-  button.addEventListener("click", myFunction);
 
 window.addEventListener('load', init);
+
 
